b9969d4dc83b90f2ad575f7a8c65ea7111b5cfc3
Mariusz Sycz <mariusz.sycz@makolab.net>
2018-09-01 19:09:04 +0200

    move all last modified to run local

---
+++ UmbracoBase4/UmbracoBase4/App_Code/publishDateSetter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+
+namespace UmbracoExample
+{
+    public class PublishDateApplicationEventHandler : ApplicationEventHandler
+    {
+        protected override void ApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            ContentService.Publishing += ContentService_Publishing;
+        }
+
+        private void ContentService_Publishing(Umbraco.Core.Publishing.IPublishingStrategy sender, Umbraco.Core.Events.PublishEventArgs<Umbraco.Core.Models.IContent> e)
+        {
+            var contentService = ApplicationContext.Current.Services.ContentService;
+            foreach (var content in e.PublishedEntities.Where(m => m.HasProperty("publishDate")))
+            {
+                var existingValue = content.GetValue("publishDate");
+                if (existingValue == null)
+                {
+                    content.SetValue("publishDate", DateTime.Now);
+                }
+            }
+        }
+    }
+}
