d7ce0d34819afaf50a94a6d85e55f807996b1e8a
Mariusz Sycz <mariusz.sycz@makolab.net>
2019-01-05 00:37:44 +0100

    update

---
+++ UmbracoBase4/UmbracoBase4/js/app.js
@@ -0,0 +1,21 @@
+﻿function Open() {
+    try {
+        el = window.document.getElementById("sidebar");
+        el.setAttribute("open", "");
+    }
+    catch (err) {
+        Log(err);
+    }
+}
+function Close() {
+    try {
+        el = window.document.getElementById("sidebar");
+        el.removeAttribute("open");
+    }
+    catch (err) {
+        Log(err);
+    }
+}
+function Log(message) {
+        console.log(message);
+}
